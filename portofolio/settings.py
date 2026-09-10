3b38283c6485935d8f14936f58a0ffe93f581fdc
AnggaRestha <anggarestha.129@gmail.com>
2026-09-10 13:31:06 +0700

    fix: database dynamic configuration for PWS

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -72,10 +72,12 @@ WSGI_APPLICATION = 'portofolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-if PRODUCTION:
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.config(
-            default=os.getenv('DATABASE_URL'),
+        'default': dj_database_url.parse(
+            DATABASE_URL,
             conn_max_age=600,
             conn_health_checks=True,
         )
