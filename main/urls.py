bfc48d677c2e5823123af11c66261ba58c642c04
AnggaRestha <anggarestha.129@gmail.com>
2026-09-16 13:22:06 +0700

    Update : Project add class

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_awards
+from main.views import create_project, show_awards
 
 from main.views import show_main, show_experience
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
     path('awards/', show_awards, name='show_awards'),
+    path("projects/add/", create_project, name="create_project"),
 ]
